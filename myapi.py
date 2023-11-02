87f82b118c0705ccfc4e4fd245ddc39e702b2d33
lawrenceuchenye <lawuche29@gmail.com>
2023-11-02 14:07:13 +0000

    bugs

--- myapi.py
+++ myapi.py
@@ -3,6 +3,21 @@ from fastapi import FastAPI
 #main app instance
 app=FastAPI()
 
+students={
+    "1":{
+        "name":"David",
+        "department":"Computer Sci.",
+        "mat":"2120/123645/regular"
+    },
+}
+
+
 @app.get("/")
 def index():
     return { "data":"started"}
+
+@app.get("/student-info/{id}/info")
+def student_info(id:int):
+    return {"student":students[st(id)]}
+
+
